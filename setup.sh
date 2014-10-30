2be2f43de5ebb4891071245876a6491d6253ed16
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-30 11:56:05 +0000

    Clone repository if needed

--- setup.sh
+++ setup.sh
@@ -7,4 +7,10 @@ sudo apt-add-repository ppa:rquillo/ansible --yes
 sudo apt-get update
 sudo apt-get install -y ansible git
 
+
+if [ ! -f standalone.yml ]; then
+    git clone https://github.com/opinsys/puavo-standalone /tmp/puavo-standalone
+    cd /tmp/puavo-standalone
+fi
+
 sudo ansible-playbook standalone.yml
