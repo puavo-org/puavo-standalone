e2d9f85e977cf85073beaae635db4dc51cfbcd8d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-30 10:59:48 +0000

    fixup

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-sudo apt-get install -y python-software-properties git
+sudo apt-get install -y python-software-properties
 sudo apt-add-repository ppa:rquillo/ansible --yes
 sudo apt-get update
-sudo apt-get install -y ansible python-apt git
+sudo apt-get install -y ansible git
 
 sudo ansible-playbook standalone.yml
