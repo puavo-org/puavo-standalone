26b6c1ab61a356b1bd3e36014aeae82d2ff8ec6b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-03 17:08:22 +0200

    Wonder if this is required anymore

--- setup.sh
+++ setup.sh
@@ -5,8 +5,7 @@ set -eux
 apt-get install -y python-software-properties
 apt-add-repository ppa:rquillo/ansible --yes
 apt-get update
-# OpenPGP data is no where to be found. Workaround with --force-yes for now
-apt-get install -y --force-yes ansible git
+apt-get install -y ansible git
 
 # If running in Vagrant use the playbook from the mount
 if [ -d /vagrant ]; then
