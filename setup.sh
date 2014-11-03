d70d0c1b245e316bda06446d8de8dd8e486abfd6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-11-03 17:10:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 set -eux
 
+# Install apt-add-repository tool
 apt-get install -y python-software-properties
+
+# Add Ansible ppa repository and install Ansible
 apt-add-repository ppa:rquillo/ansible --yes
 apt-get update
 apt-get install -y ansible git
@@ -14,14 +17,16 @@ fi
 
 # No we don't have the playbook fetch it
 if [ ! -f standalone.yml ]; then
-    # If already fetched update it
     if [ -d /tmp/puavo-standalone ]; then
+        # If already fetched update it
         cd /tmp/puavo-standalone
         git pull
     else
+        # Otherwise get the latest rules
         git clone https://github.com/opinsys/puavo-standalone /tmp/puavo-standalone
         cd /tmp/puavo-standalone
     fi
 fi
 
+# Finally apply the rules to localhost
 ansible-playbook -i local.inventory standalone.yml
